67d470d185cefe5c3a842d8ac436c6df505c49a0
Nil Borodulia <brdnlsrg@gmail.com>
2017-12-11 20:33:15 +0200

    remove all

--- backend/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-define('PATH_SCRIPT', dirname(__FILE__));
-define('PATH_STATIC_PAGE',  dirname(__FILE__).'/../');
-
-define('PATH_GENERATE_TEMPLATE', dirname(__FILE__).'/site_templates/');
--- backend/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-if (!class_exists('nilBackedController')) {
-    include_once dirname(__FILE__)."/core/nilBackedController.php";
-}
-$backend = new nilBackendController();
-
-$auth = $backend->auth('admin', 'admin');
